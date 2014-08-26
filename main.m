a1faae73041e9005226b691035cdfeb91173922d
kmdouglass <kyle.m.douglass@gmail.com>
2014-08-26 14:07:32 +0200

    main.m now processes all data.

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 % This script should be run after the analysis workflow is determined from
 % data_mining.m.
 %
-% $AUTHOR: Kyle M. Douglass $ $DATE: 2014/08/25 $ $REVISION: 0.8 $
+% $AUTHOR: Kyle M. Douglass $ $DATE: 2014/08/26 $ $REVISION: 0.8 $
 %
 %% Use parallel processing to speed up computation? (use 'false' if unsure)
 useParallel = true;
@@ -25,38 +25,226 @@ minLoc = 50;
 % All datasets are comparisons between two populations, typically Hela L
 % cells and Hela S cells, or between two different transfections.
 % Therefore, each experiment consists of two datasets.
-data(1).shortName = '11-08-2014 pSuper';
-data(1).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
-data(1).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pSuper_non filtered/';
-data(1).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pSuper_non filtered/';
+
+%==========================================================================
+% 11-06-2014 FISH Original data
+%==========================================================================
+data(1).shortName = '11-06-2014 FISH';
+data(1).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Verena/11_06_2014_FISH_HelaS_L/11_06_2014_FISH_Hela_S_L/';
+data(1).dataset1Dir = '11_06_2014_Hela_L_FISH/Hela L FISH molecule lists/';
+data(1).dataset2Dir = '11_06_2014_Hela_S_FISH/Hela S FISH molecule lists/';
 data(1).dataset1ShortName = 'L dataset';
 data(1).dataset2ShortName = 'S dataset';
 data(1).distributions1 = [];
 data(1).distributions2 = [];
 data(1).fits = [];
 
-data(2).shortName = '11-08-2014 pLVP041';
-data(2).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
-data(2).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pLVP041_non filtered/';
-data(2).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pLVP041_non filtered/';
+%==========================================================================
+% 29-07-2014 SmchD1
+%==========================================================================
+data(2).shortName = '29-07-2014 SmchD1 pSuper';
+data(2).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/29_07_2014_HelaS_L_SmchD1_KD_FISH/29_07_2014_HelaS_L_SmchD1_KD_FISH_Mol_lists/';
+data(2).dataset1Dir = '29_07_2014_HelaL_SmchD1_pSuper_mol_list/';
+data(2).dataset2Dir = '29_07_2014_HelaS_SmchD1_pSuper_mol_list/';
 data(2).dataset1ShortName = 'L dataset';
 data(2).dataset2ShortName = 'S dataset';
 data(2).distributions1 = [];
 data(2).distributions2 = [];
 data(2).fits = [];
 
-data(3).shortName = '11-08-2014 pLVP042';
-data(3).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
-data(3).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pLVP042_non filtered/';
-data(3).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pLVP042_non filtered/';
+data(3).shortName = '29-07-2014 SmchD1 pLVP041';
+data(3).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/29_07_2014_HelaS_L_SmchD1_KD_FISH/29_07_2014_HelaS_L_SmchD1_KD_FISH_Mol_lists/';
+data(3).dataset1Dir = '29_07_2014_HelaL_SmchD1_pLVP041_mol_list/';
+data(3).dataset2Dir = '29_07_2014_HelaS_SmchD1_pLVP041_mol_list/';
 data(3).dataset1ShortName = 'L dataset';
 data(3).dataset2ShortName = 'S dataset';
 data(3).distributions1 = [];
 data(3).distributions2 = [];
 data(3).fits = [];
 
+data(4).shortName = '29-07-2014 SmchD1 pLVP042';
+data(4).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/29_07_2014_HelaS_L_SmchD1_KD_FISH/29_07_2014_HelaS_L_SmchD1_KD_FISH_Mol_lists/';
+data(4).dataset1Dir = '29_07_2014_HelaL_SmchD1_pLVP042_mol_list/';
+data(4).dataset2Dir = '29_07_2014_HelaS_SmchD1_pLVP042_mol_list/';
+data(4).dataset1ShortName = 'L dataset';
+data(4).dataset2ShortName = 'S dataset';
+data(4).distributions1 = [];
+data(4).distributions2 = [];
+data(4).fits = [];
+
+%==========================================================================
+% 30-07-2014 SmchD1
+%==========================================================================
+data(5).shortName = '30-07-2014 SmchD1 pSuper';
+data(5).rootDir = '/media/My Book/Kyle/Telomere_Data/30_07_2014_HeLaS_L_SmchD1_KD_FISH/';
+data(5).dataset1Dir = '30_07_2014_HelaL_SmchD1_pSuper_mol_list/';
+data(5).dataset2Dir = '30_07_2014_HelaS_SmchD1_pSuper_mol_list/';
+data(5).dataset1ShortName = 'L dataset';
+data(5).dataset2ShortName = 'S dataset';
+data(5).distributions1 = [];
+data(5).distributions2 = [];
+data(5).fits = [];
+
+data(6).shortName = '30-07-2014 SmchD1 pLVP041';
+data(6).rootDir = '/media/My Book/Kyle/Telomere_Data/30_07_2014_HeLaS_L_SmchD1_KD_FISH/';
+data(6).dataset1Dir = '30_07_2014_HelaL_SmchD1_pLVP041_mol_list/';
+data(6).dataset2Dir = '30_07_2014_HelaS_SmchD1_pLVP041_mol_list/';
+data(6).dataset1ShortName = 'L dataset';
+data(6).dataset2ShortName = 'S dataset';
+data(6).distributions1 = [];
+data(6).distributions2 = [];
+data(6).fits = [];
+
+data(7).shortName = '30-07-2014 SmchD1 pLVP042';
+data(7).rootDir = '/media/My Book/Kyle/Telomere_Data/30_07_2014_HeLaS_L_SmchD1_KD_FISH/';
+data(7).dataset1Dir = '30_07_2014_HelaL_SmchD1_pLVP042_mol_list/';
+data(7).dataset2Dir = '30_07_2014_HelaS_SmchD1_pLVP042_mol_list/';
+data(7).dataset1ShortName = 'L dataset';
+data(7).dataset2ShortName = 'S dataset';
+data(7).distributions1 = [];
+data(7).distributions2 = [];
+data(7).fits = [];
+
+%==========================================================================
+% 05-08-2014 Hela L LRif1
+%==========================================================================
+data(8).shortName = '05-08-2014 Hela L siGfp';
+data(8).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/05_08_2014_HelaL_LRif1_KD_FISH/05_08_2014_HelaL_LRif1_Mol_Lists/';
+data(8).dataset1Dir = '05_08_2014_HelaL_LRif1_siGfp_mol_lists/';
+data(8).dataset2Dir = '05_08_2014_HelaL_LRif1_siGfp_tr2_mol_lists/';
+data(8).dataset1ShortName = '1st transfection';
+data(8).dataset2ShortName = '2nd transfection';
+data(8).distributions1 = [];
+data(8).distributions2 = [];
+data(8).fits = [];
+
+data(9).shortName = '05-08-2014 Hela L si1LRif1';
+data(9).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/05_08_2014_HelaL_LRif1_KD_FISH/05_08_2014_HelaL_LRif1_Mol_Lists/';
+data(9).dataset1Dir = '05_08_2014_HelaL_LRif1_si1LRif1_mol_lists/';
+data(9).dataset2Dir = '05_08_2014_HelaL_LRif1_si1LRif1_tr2_mol_lists/';
+data(9).dataset1ShortName = '1st transfection';
+data(9).dataset2ShortName = '2nd transfection';
+data(9).distributions1 = [];
+data(9).distributions2 = [];
+data(9).fits = [];
+
+data(10).shortName = '05-08-2014 Hela L si6LRif1';
+data(10).rootDir = '/mnt/LEBSRV/Michael-Kyle-Douglass/Telo2/05_08_2014_HelaL_LRif1_KD_FISH/05_08_2014_HelaL_LRif1_Mol_Lists/';
+data(10).dataset1Dir = '05_08_2014_HelaL_LRif1_si6LRif1_mol_lists/';
+data(10).dataset2Dir = '05_08_2014_HelaL_LRif1_si6LRif1_tr2_mol_lists/';
+data(10).dataset1ShortName = '1st transfection';
+data(10).dataset2ShortName = '2nd transfection';
+data(10).distributions1 = [];
+data(10).distributions2 = [];
+data(10).fits = [];
+
+%==========================================================================
+% 11-08-2014 TrF2
+%==========================================================================
+data(11).shortName = '11-08-2014 pSuper';
+data(11).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
+data(11).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pSuper_non filtered/';
+data(11).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pSuper_non filtered/';
+data(11).dataset1ShortName = 'L dataset';
+data(11).dataset2ShortName = 'S dataset';
+data(11).distributions1 = [];
+data(11).distributions2 = [];
+data(11).fits = [];
+
+data(12).shortName = '11-08-2014 pLVP041';
+data(12).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
+data(12).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pLVP041_non filtered/';
+data(12).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pLVP041_non filtered/';
+data(12).dataset1ShortName = 'L dataset';
+data(12).dataset2ShortName = 'S dataset';
+data(12).distributions1 = [];
+data(12).distributions2 = [];
+data(12).fits = [];
+
+data(13).shortName = '11-08-2014 pLVP042';
+data(13).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
+data(13).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pLVP042_non filtered/';
+data(13).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pLVP042_non filtered/';
+data(13).dataset1ShortName = 'L dataset';
+data(13).dataset2ShortName = 'S dataset';
+data(13).distributions1 = [];
+data(13).distributions2 = [];
+data(13).fits = [];
+
+data(14).shortName = '11-08-2014 TrF2';
+data(14).rootDir = '/media/My Book/Kyle/Telomere_Data/11_08_2014_HelaS_L_SmchD1_TRf2_KD_FISH/11_08_2014_HeLaL_S_SMCHD1_Trf2_KD_FISH_Molecule lists/';
+data(14).dataset1Dir = '11_08_2014_HeLaL_KD_Smchd1_TRF2_pLVP TRF2_non filtered/';
+data(14).dataset2Dir = '11_08_2014_HeLaS_KD_Smchd1_TRF2_pLVP TRF2_non filtered/';
+data(14).dataset1ShortName = 'L dataset';
+data(14).dataset2ShortName = 'S dataset';
+data(14).distributions1 = [];
+data(14).distributions2 = [];
+data(14).fits = [];
+
+%==========================================================================
+% 13-08-2014 TrF2
+%==========================================================================
+data(15).shortName = '13-08-2014 pSuper';
+data(15).rootDir = '/media/My Book/Kyle/Telomere_Data/13_08_2014_HelaS_L_SmchD1_Trf2_KD_FISH/13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_Mol_list_not_filtered/';
+data(15).dataset1Dir = '13_08_2014_HelaL_SmchD1_TRF2_KD_FISH_pSuper_Mol_list_not_filtered/';
+data(15).dataset2Dir = '13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_pSuper_Mol_list_not_filtered/';
+data(15).dataset1ShortName = 'L dataset';
+data(15).dataset2ShortName = 'S dataset';
+data(15).distributions1 = [];
+data(15).distributions2 = [];
+data(15).fits = [];
+
+data(16).shortName = '13-08-2014 pLVP041';
+data(16).rootDir = '/media/My Book/Kyle/Telomere_Data/13_08_2014_HelaS_L_SmchD1_Trf2_KD_FISH/13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_Mol_list_not_filtered/';
+data(16).dataset1Dir = '13_08_2014_HelaL_SmchD1_TRF2_KD_FISH_pLVP041_Mol_list_not_filtered/';
+data(16).dataset2Dir = '13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_pLVP041_Mol_list_not_filtered/';
+data(16).dataset1ShortName = 'L dataset';
+data(16).dataset2ShortName = 'S dataset';
+data(16).distributions1 = [];
+data(16).distributions2 = [];
+data(16).fits = [];
+
+data(17).shortName = '13-08-2014 pLVP042';
+data(17).rootDir = '/media/My Book/Kyle/Telomere_Data/13_08_2014_HelaS_L_SmchD1_Trf2_KD_FISH/13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_Mol_list_not_filtered/';
+data(17).dataset1Dir = '13_08_2014_HelaL_SmchD1_TRF2_KD_FISH_pLVP042_Mol_list_not_filtered/';
+data(17).dataset2Dir = '13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_pLVP042_Mol_list_not_filtered/';
+data(17).dataset1ShortName = 'L dataset';
+data(17).dataset2ShortName = 'S dataset';
+data(17).distributions1 = [];
+data(17).distributions2 = [];
+data(17).fits = [];
+
+data(18).shortName = '13-08-2014 TrF2';
+data(18).rootDir = '/media/My Book/Kyle/Telomere_Data/13_08_2014_HelaS_L_SmchD1_Trf2_KD_FISH/13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_Mol_list_not_filtered/';
+data(18).dataset1Dir = '13_08_2014_HelaL_SmchD1_TRF2_KD_FISH_TRF2_Mol_list_not_filtered/';
+data(18).dataset2Dir = '13_08_2014_HelaS_SmchD1_TRF2_KD_FISH_TRF2_Mol_list_not_filtered/';
+data(18).dataset1ShortName = 'L dataset';
+data(18).dataset2ShortName = 'S dataset';
+data(18).distributions1 = [];
+data(18).distributions2 = [];
+data(18).fits = [];
+
+%% Verify that all directories are correct.
+for dirCtr = 1:length(data)
+    completeDir1 = [data(dirCtr).rootDir data(dirCtr).dataset1Dir];
+    completeDir2 = [data(dirCtr).rootDir data(dirCtr).dataset2Dir];
+    
+    A = exist(completeDir1, 'dir');
+    B = exist(completeDir2, 'dir');
+    
+    if A == 0
+        error(['Error: directory ' completeDir1 ' does not exist.'])
+    elseif B == 0
+        error(['Error: directory ' completeDir2 ' does not exist.'])
+    end
+end
+clear dirCtr
+
+%% Primary processing loop.
+
 for dataCtr = 1:length(data)
 % Loops over all the data files defined above.
+disp(['Processing dataset ' data(dataCtr).shortName])
 
 completeDir1 = [data(dataCtr).rootDir data(dataCtr).dataset1Dir];
 completeDir2 = [data(dataCtr).rootDir data(dataCtr).dataset2Dir];
@@ -81,7 +269,6 @@ SProcData(length(SFiles),1).M2 = 0;
 SProcData(length(SFiles),1).M2Mag = 0;
 SProcData(length(SFiles),1).numLoc = 0;
 SProcData(length(SFiles),1).volume = 0;
-
 
 % process_data(fileName) is custom function call.
 if useParallel
@@ -149,6 +336,7 @@ for ctr = 1:numel(fields)
 end
 
 % Write distributions out to the external data structure.
+clear distributions1 distributions2
 data(dataCtr).distributions1 = LAllData;
 data(dataCtr).distributions2 = SAllData;
 
@@ -156,6 +344,8 @@ data(dataCtr).distributions2 = SAllData;
 close all
 [LN, LBIN] = histc(LAllData.M2, bins.M2);
 [SN, SBIN] = histc(SAllData.M2, bins.M2);
+
+disp(['Distributions for data set ' data(dataCtr).shortName])
 
 % Set the size of the figure window.
 subplot(2,2,1)
@@ -239,9 +429,11 @@ grid on
 % squares fitting to a power law, see:
 % <http://mathworld.wolfram.com/LeastSquaresFittingPowerLaw.html>
 
-% Work with the first dataset.
 f = fittype('a*x.^b');
 
+disp(['Scaling for data set ' data(dataCtr).shortName])
+
+% Work with the first dataset.
 FigHandle = figure('Position', [100, 100, 800, 800]);
 subplot(2,1,1)
 x1 = LAllData.numLoc;
@@ -265,14 +457,13 @@ plot(fitRobust1,'g-')
 legend( 'Data', 'Outliers', 'Original fit', 'Fit with outliers excluded', 'Robust fit' )
 hold off
 grid on
-title('L dataset') % CHANGE TO VARIABLE
+title(data(dataCtr).dataset1ShortName)
 xlabel('Number of localizations')
 ylabel('R_g, nm')
 xlim([0 700])
 ylim([0 300])
 
 % Work with the second dataset.
-pause(1)
 subplot(2,1,2)
 x2 = SAllData.numLoc;
 y2 = SAllData.M2Mag;
@@ -288,6 +479,7 @@ fitNoOutliers2 = fit(x2,y2,f,'StartPoint', [17 0.33], 'Exclude',outliers);
 
 fitRobust2 = fit(x2,y2,f,'StartPoint',[17 0.33],'Robust','on');
 
+pause(1) % Ensures that the fit has time to work before plotted.
 plot(fit2,'b-', x2, y2, 'k.', outliers, 'm*')
 hold on
 plot(fitNoOutliers2,'r--')
@@ -295,13 +487,14 @@ plot(fitRobust2,'g-')
 legend( 'Data', 'Outliers', 'Original fit', 'Fit with outliers excluded', 'Robust fit' )
 hold off
 grid on
-title('S dataset') % CHANGE TO VARIABLE
+title(data(dataCtr).dataset2ShortName)
 xlabel('Number of localizations')
 ylabel('R_g, nm')
 xlim([0 700])
 ylim([0 300])
 
 % Store fits in the external data array.
+clear fits
 data(dataCtr).fits.fit1 = fit1;
 data(dataCtr).fits.fitNoOutliers1 = fitNoOutliers1;
 data(dataCtr).fits.fitRobust1 = fitRobust1;
